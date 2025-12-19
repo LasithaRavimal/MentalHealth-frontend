9ceaa8702cf5ea4cbac154fb59153e5becf85c8b
LasithaRavimal <lasithravimal7@gmail.com>
2025-12-19 22:00:26 +0530

    PlayerLayout component structure and improve readability

--- src/components/music/PlayerLayout.jsx
+++ src/components/music/PlayerLayout.jsx
@@ -26,3 +26,51 @@ const Layout = ({ children }) => {
     prediction,
     handlePlayPause,
   } = usePlayer();
+
+  return (
+    <div className="flex h-screen bg-spotify-black overflow-hidden">
+      <Sidebar />
+
+      <div className="flex-1 flex flex-col overflow-hidden bg-spotify-dark-gray pb-24">
+        <TopBar />
+        {children}
+      </div>
+
+      <PlayerBar />
+
+      {showExpanded && currentSong && (
+        <PlayerExpanded
+          song={currentSong}
+          isPlaying={isPlaying}
+          onPlayPause={handlePlayPause}
+          onClose={() => setShowExpanded(false)}
+          currentTime={currentTime}
+          duration={duration}
+          volume={volume}
+          onVolumeChange={handleVolumeChange}
+          onSeek={(time) => {
+            if (audioRef.current) {
+              audioRef.current.currentTime = time;
+            }
+            setCurrentTime(time);
+          }}
+        />
+      )}
+
+      {showPredictionModal && prediction && (
+        <PredictionModal
+          isOpen={showPredictionModal}
+          onClose={() => setShowPredictionModal(false)}
+          prediction={prediction}
+          onViewHistory={() => {
+            setShowPredictionModal(false);
+            navigate('/sessions');
+          }}
+        />
+      )}
+    </div>
+  );
+};
+
+export default Layout;
+
