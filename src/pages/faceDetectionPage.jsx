52a5ecce47f6325c472cd9848730167ee7fbe357
Sajithjay99 <sajithjay99@gmail.com>
2025-12-19 21:40:41 +0530

    Add Face Detection Page and integrate with routing

---
+++ src/pages/faceDetectionPage.jsx
@@ -0,0 +1,9 @@
+const FaceDetectionPage = () => {
+  return (
+    <div className="flex items-center justify-center min-h-screen text-white bg-spotify-black">
+      Face Detection Page (coming soon)
+    </div>
+  );
+};
+
+export default FaceDetectionPage;
