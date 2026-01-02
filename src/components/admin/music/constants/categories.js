e6824593d0fd0a9dd94ee083f73f151792ef268f
LasithaRavimal <lasithravimal7@gmail.com>
2026-01-02 21:26:22 +0530

    Implement Song Management component with upload, edit, delete, and filtering functionalities

---
+++ src/components/admin/music/constants/categories.js
@@ -0,0 +1,36 @@
+// Predefined song categories for admin use
+export const SONG_CATEGORIES = [
+  'Sad',
+  'Calm',
+  'Happy',
+  'Energetic',
+  'Relaxing',
+  'Motivating',
+  'Melancholic',
+  'Uplifting',
+  'Peaceful',
+  'Inspirational',
+  'Focus',
+  'Sleep',
+  'Workout',
+  'Party',
+  'Romantic',
+  'Meditation',
+  'Ambient',
+  'Jazz',
+  'Classical',
+  'Rock',
+  'Pop',
+  'Electronic',
+  'Acoustic',
+  'Instrumental',
+  'Nature',
+  'Chill',
+  'Upbeat',
+  'Soulful',
+  'Dreamy',
+  'Intense',
+];
+
+export default SONG_CATEGORIES;
+
